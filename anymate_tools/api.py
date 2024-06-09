274515ccca029ec4f0eb12897ba81b5a4c78fb3a
QuickLAW <89532126+QuickLAW@users.noreply.github.com>
2024-06-09 23:43:28 +0800

    Delete anymate_tools directory

--- anymate_tools/api.py
+++
@@ -1,5 +0,0 @@
-
-get_explore_api = 'https://api.any-mate.com/v1/Api/MomentExplorePage'
-get_last_post_api = 'https://api.any-mate.com/v1/Api/MateMomentPage'
-get_info_api1 = ' https://api.any-mate.com/v1/Api/Mate/'
-search_api = 'https://api.any-mate.com/v1/Api/MateSearchPage'
