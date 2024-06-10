2a3de226a7449f16301cb1aacd5ea97516063a87
QuickLAW <89532126+QuickLAW@users.noreply.github.com>
2024-06-10 18:09:26 +0800

    修正错误

--- nonebot_plugin_anymate/api.py
+++ nonebot_plugin_anymate/api.py
@@ -1,5 +1,5 @@
 
 get_explore_api = 'https://api.any-mate.com/v1/Api/MomentExplorePage'
 get_last_post_api = 'https://api.any-mate.com/v1/Api/MateMomentPage'
-get_info_api1 = ' https://api.any-mate.com/v1/Api/Mate/'
+get_info_api1 = 'https://api.any-mate.com/v1/Api/Mate/'
 search_api = 'https://api.any-mate.com/v1/Api/MateSearchPage'
