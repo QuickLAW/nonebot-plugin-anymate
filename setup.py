3064d570759a77a40429507ccc3c0d02b87c7eb1
QuickLAW <89532126+QuickLAW@users.noreply.github.com>
2024-07-29 22:39:59 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="nonebot_plugin_anymate",  # 包名
-    version="1.4.0.1",  # 版本号
+    version="1.5.8.0",  # 版本号
     description="QQ上使用的anymate网站小工具",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -15,17 +15,24 @@ setup(
     author_email="yewillwork@outlook.com",
     url="https://github.com/QuickLAW/nonebot-plugin-anymate",
     install_requires=[
-        "nonebot2",
+        "anyio",
+        "filetype",
+        "strenum",
         "pytz",
         "httpx",
         "pydantic",
-        "nonebot-adapter-onebot",
+        "nonebot_adapter_onebot",
         "aiosqlite",
         "nonebot-plugin-send-anything-anywhere",
+        "nonebot_plugin_apscheduler",
+        "nonebot2",
     ],
     license="BSD 3-Clause License",
-    packages=find_packages(),
-    platforms=["~onebot.v11"],
+    packages=find_packages(
+        include=["nonebot_plugin_anymate", "nonebot_plugin_anymate.*"]
+    ),
+    include_package_data=True,
+    platforms=["all"],
     classifiers=[
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
